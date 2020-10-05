f9085deb2014f0dc3cc58e942dad13afa015d0d9
arlukens20 <Drew4818@gmail.com>
2020-10-05 14:40:26 -0400

    Added Drop Down menu on Nav Bar

--- src/app/components/start-load-test-widget/start-load-test-widget.component.ts
+++ src/app/components/start-load-test-widget/start-load-test-widget.component.ts
@@ -1,57 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import loadTestConfig from '../../models/loadTestConfig'
-import { FormGroup, FormControl } from '@angular/forms'
-/*import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormGroup, FormControl } from '@angular/forms';
+/* import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatInputModule } from '@angular/material/input';*/
+import { MatInputModule } from '@angular/material/input'; */
 
 import { HttpClient } from '@angular/common/http';
-/*import { GraphRoute } from '../../state/graph-route/graph-route.model';
+import LoadTestConfig from '../../models/LoadTestConfig';
+/* import { GraphRoute } from '../../state/graph-route/graph-route.model';
 import { UPDATE_ROUTE } from '../../state/graph-route/graph-route.actions';
-import { Store } from '@ngrx/store';*/
+import { Store } from '@ngrx/store'; */
 
 @Component({
   selector: 'app-start-load-test-widget',
   templateUrl: './start-load-test-widget.component.html',
-  styleUrls: ['./start-load-test-widget.component.scss']
+  styleUrls: ['./start-load-test-widget.component.scss'],
 })
-export class StartLoadTestWidgetComponent implements OnInit {
-  time: number = 0;
+export default class StartLoadTestWidgetComponent implements OnInit {
+  time = 0;
+
   interval;
-  running: boolean = false;
-  advance: boolean = false;
-  indexValue: number = 0;
-  private Base_Url = "http://localhost:8083/Docutest";
+
+  running = false;
+
+  advance = false;
+
+  indexValue = 0;
+  LFC;
+
+  private Base_Url = 'http://localhost:8083/Docutest';
 
   AdvanceForm = new FormGroup({
     planName: new FormControl(''),
     loops: new FormControl(''),
     duration: new FormControl(''),
     thread: new FormControl(''),
-    rampUp: new FormControl('')
+    rampUp: new FormControl(''),
   });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   startTest() {
-    var lfc;
-    if (this.advance == true) {
-      lfc = new loadTestConfig(
+    if (this.advance === true) {
+      this.LFC = new LoadTestConfig(
         this.AdvanceForm.get('planName').value,
         this.AdvanceForm.get('loops').value,
         this.AdvanceForm.get('duration').value,
         this.AdvanceForm.get('thread').value,
         this.AdvanceForm.get('rampUp').value,
       );
-      console.log(lfc);
+      console.log(this.LFC);
     } else {
-      lfc = new loadTestConfig("Default", 123, 123, 123, 123);
-      console.log(lfc)
+      this.LFC = new LoadTestConfig('Default', 123, 123, 123, 123);
+      console.log(this.LFC);
     }
-    //this.submit(lfc);
+    // this.submit(lfc);
     this.startTimer();
     this.running = true;
   }
@@ -59,11 +64,11 @@ export class StartLoadTestWidgetComponent implements OnInit {
   startTimer() {
     if (this.interval == null) {
       this.interval = setInterval(() => {
-        this.time++;
+        this.time += 1;
         if (this.time > 200) {
           this.running = false;
         }
-      }, 1000)
+      }, 1000);
     }
   }
 
@@ -74,8 +79,9 @@ export class StartLoadTestWidgetComponent implements OnInit {
   submit(item: any) {
     const formData = new FormData();
     formData.append('file', sessionStorage.getItem('file'));
-    this.http.post<any>(this.Base_Url + '/upload', formData).subscribe(
+    this.http.post<any>(`${this.Base_Url}/upload`, formData).subscribe(
       (res) => console.log(res),
-      (err) => console.log(err));
+      (err) => console.log(err),
+    );
   }
 }
