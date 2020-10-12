de137c0fc028adcd28e8c511b05628000df6c615
Garrett Neff <WGNeff0@gmail.com>
2020-10-12 16:07:13 -0400

    Added graphs and made new dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { CssDashboardComponent } from './components/css-dashboard/css-dashboard.component';
 
 const routes: Routes = [
   { path: 'upload', component: FileUploadComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'cssdash', component: CssDashboardComponent }
 ];
 
 @NgModule({
